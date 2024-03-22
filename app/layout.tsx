78a7e7d3726c8f9932c8d1de35fb98b2b24a22f0
Devroy Blake <necrodevz@gmail.com>
2024-03-22 00:57:47 -0500

    update layout.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,26 +16,9 @@ export default function RootLayout({
 }) {    
   return (
       <html lang="en">
-          <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-          <style type="text/tailwindcss">
-              @layer utilities {
-                  .content - auto {
-                              content - visibility: auto;
-                  }
-                }
-          </style>
+
           <body className={inter.className}>{children}</body>
-          <script>
-              tailwind.config = {
-                        theme: {
-                            extend: {
-                                colors: {
-                                    clifford: '#da373d',
-                                }
-                            }
-                        }
-                    }
-          </script>
+
     </html>
   )
 }
